55464e424815d4ba1644158a669154e69cfb541a
Arshi <fahrezzi@widianto.org>
2025-04-17 09:32:43 +0700

    Changes: Merubah tampilan dari Navbar.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,31 +17,61 @@
                     <span class="navbar-toggler-line"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                    <ul class="navbar-nav alt-font"> 
-                        <li class="nav-item"><a href="demo-finance.html" class="nav-link">Home</a></li>
-                        <li class="nav-item"><a href="demo-finance-about.html" class="nav-link">About</a></li>
+                    <ul class="navbar-nav alt-font">
+                        <li class="nav-item"><a href="demo-finance-about.html" class="nav-link">Beranda</a></li>
                         <li class="nav-item dropdown simple-dropdown">
-                            <a href="demo-finance-expertise.html" class="nav-link">Expertise</a>
+                            <a href="demo-finance-expertise.html" class="nav-link">Tentang Kami</a>
                             <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink_1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> 
-                                <li><a href="demo-finance-expertise-details.html">Finance planning</a></li>
-                                <li><a href="demo-finance-expertise-details.html">Wealth management</a></li>
-                                <li><a href="demo-finance-expertise-details.html">Strategic planning</a></li>
-                                <li><a href="demo-finance-expertise-details.html">Audit assurance</a></li>
+                                <li><a href="demo-finance-expertise-details.html">Tentang Administrasi Bisnis</a></li>
+                                <li><a href="demo-finance-expertise-details.html">Visi, Misi, dan Tujuan</a></li>
+                                <li><a href="demo-finance-expertise-details.html">Struktur Organisasi</a></li>
+                                <li><a href="demo-finance-expertise-details.html">Fasilitas</a></li>
+                                <li><a href="demo-finance-expertise-details.html">Dosen</a></li>
+                                <li><a href="demo-finance-expertise-details.html">Akreditas</a></li>
+                                <li><a href="demo-finance-expertise-details.html">Karir</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item"><a href="demo-finance-team.html" class="nav-link">Team</a></li>
-                        <li class="nav-item"><a href="demo-finance-pricing.html" class="nav-link">Pricing</a></li>
-                        <li class="nav-item"><a href="demo-finance-news.html" class="nav-link">News</a></li>
-                        <li class="nav-item"><a href="demo-finance-contact.html" class="nav-link">Contact</a></li>
+                        <li class="nav-item dropdown simple-dropdown">
+                            <a href="demo-finance-team.html" class="nav-link">Akademik</a>
+                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink_1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> 
+                                <li><a href="demo-finance-team.html">Kurikulum</a></li>
+                                <li><a href="demo-finance-team.html">Profil Lulusan</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown simple-dropdown">
+                            <a href="demo-finance-pricing.html" class="nav-link">Publikasi</a>
+                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink_1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> 
+                                <li><a href="demo-finance-pricing.html">Artikel</a></li>
+                                <li><a href="demo-finance-pricing.html">Berita</a></li>
+                                <li><a href="demo-finance-pricing.html">Kegiatan</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown simple-dropdown">
+                            <a href="demo-finance-contact.html" class="nav-link">Daftar Sekarang!</a>
+                            <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink_1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a href="demo-finance-contact.html">Pendaftaran</a></li>
+                                <li><a href="demo-finance-contact.html">Jadwal Penerimaan</a></li>
+                                <li><a href="demo-finance-contact.html">Persyaratan Masuk</a></li>
+                            </ul>   
+                        </li>
                     </ul>
                 </div>
             </div>
             <div class="col-auto col-lg-2 text-end d-none d-sm-flex"> 
                 <div class="header-icon">
-                    <div class="header-button"><a href="#contact-form" class="btn btn-very-small btn-white btn-box-shadow btn-round-edge left-icon text-transform-none popup-with-form"><i class="feather icon-feather-mail"></i>Free quote</a></div>
+                    <li class="dropdwon simple-dropdown">
+                        <div class="header-button"><a href="#contact-form" class="btn btn-very-small btn-white btn-box-shadow btn-round-edge left-icon text-transform-none popup-with-form"><i class="feather icon-feather-mail"></i>Menu</a></div>
+                        <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink_1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a href="demo-finance-contact.html">LMS IWU</a></li>
+                        </ul>
+                    </li>
                 </div>
-                <!-- start contact form -->
+                {{-- <!-- start contact form -->
                 <div id="contact-form" class="container p-0 contact-form-style-01 position-relative text-center mfp-hide">
                     <div class="row g-0">
                         <div class="col-lg-5 cover-background md-h-600px xs-h-400px" style="background-image:url('https://via.placeholder.com/1082x1082');"></div>
@@ -74,7 +104,7 @@
                         </div>
                     </div>
                 </div>
-                <!-- end contact form -->
+                <!-- end contact form --> --}}
             </div>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('about');
+    return view('home');
 });
 
-Route::get('/home', function (){
+Route::get('#', function (){
     return view('demo-finance');
 });
