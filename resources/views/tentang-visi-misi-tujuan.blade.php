3d7c1aa8b14bf0093a0db5b4ad8561f24e5480c6
Arshi <fahrezzi@widianto.org>
2025-04-21 11:41:11 +0700

    Add: Mengubah tampilan tabel dan juga card.

--- resources/views/tentang-visi-misi-tujuan.blade.php
+++ resources/views/tentang-visi-misi-tujuan.blade.php
@@ -28,34 +28,91 @@
 <!-- end page title -->
 {{-- start card --}}
         <section>
-            <div class="tw-w-full tw-max-w-xl tw-mx-auto tw-p-4 tw-bg-white tw-shadow-lg tw-rounded-xl">
+            <div class="tw-w-full tw-max-w-7xl tw-min-h-[600px] tw-mx-auto tw-p-6 tw-bg-gray-100 tw-shadow-lg tw-rounded-xl">
                 <h3 class="tw-mb-4 tw-text-center tw-text-black">Visi, Misi, Tujuan</h3>
                 <div class="tw-flex tw-border-b">
                   <button class="tab-button tw-px-4 tw-py-2 tw-text-[#BC8947] hover:tw-text-[#c9a06b] tw-font-semibold tw-border-b-2 tw-border-[#BC8947]" data-tab="visi">
-                    About
+                    Visi
                   </button>
                   <button class="tab-button tw-px-4 tw-py-2 tw-text-gray-600 hover:tw-text-[#c9a06b]" data-tab="misi">
-                    Services
+                    Misi
                   </button>
                   <button class="tab-button tw-px-4 tw-py-2 tw-text-gray-600 hover:tw-text-[#c9a06b]" data-tab="tujuan">
-                    Contact
+                    Tujuan
                   </button>
                 </div>
               
-                <div class="tab-content tw-p-4" data-anime="{ &quot;translateY&quot;: [30, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 400, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 200, &quot;easing&quot;: &quot;easeOutQuad&quot; }" id="visi">
-                  <h2 class="tw-text-3xl tw-text-[#BC8947] tw-mb-2 tw-font-medium">Visi</h2>
-                  <p class="tw-text-gray-600">Menjadi Program Studi Administrasi Bisnis yang unggul dalam meningkatkan derajat kemanusiaan dan pemberdayaan perempuan melalui rekayasa hayati untuk menghasilkan lulusan yang profesional, berkarakter, dan berjiwa entrepreneur dalam pengembangan dan penerapan keilmuan Administrasi Bisnis yang mampu bersaing secara global di tahun 2035.</p>
+                <div class="tab-content tw-p-4 tw-min-h-[400px]" data-anime="{ &quot;translateY&quot;: [30, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 400, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 200, &quot;easing&quot;: &quot;easeOutQuad&quot; }" id="visi">
+                    <div class="tw-overflow-x-auto">
+                        <table class="tw-w-full tw-text-lg tw-text-left tw-text-gray-900 tw-min-h-[400px]">
+                          <thead class="tw-text-lg tw-text-[#BC8947] tw-uppercase tw-bg-slate-200">
+                            <tr>
+                              <th scope="col" class="tw-pl-6 tw-py-3">No</th>
+                              <th scope="col" class="tw-pr-6 tw-py-3 tw-text-center">Visi</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr class="tw-bg-white tw-border-b tw-border-gray-400">
+                              <td class="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900">1</td>
+                              <td class="tw-px-6 tw-py-4">	
+                                Menjadi Program Studi Teknik Informatika yang unggul dalam meningkatkan derajat kemanusiaan dan pemberdayaan perempuan melalui rekayasa hayati untuk menghasilkan lulusan yang profesional, berkarakter, dan berjiwa entrepreneur dalam pengembangan dan penerapan keilmuan Teknik Informatika yang mampu bersaing secara global di tahun 2035.</td>
+                            </tr>
+                            <tr class="tw-bg-white tw-border-b tw-border-gray-400 last:tw-border-0">
+                              <td class="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900">2</td>
+                              <td class="tw-px-6 tw-py-4">Menjadi program studi unggulan dalam bidang informatika yang inovatif, berdaya saing global, serta berkontribusi dalam pengembangan ilmu pengetahuan dan teknologi untuk kemajuan masyarakat. </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>    
                 </div>
               
                 <div class="tab-content tw-p-4 tw-hidden" id="misi">
-                  <h2 class="tw-text-3xl tw-text-[#BC8947] tw-mb-2 tw-font-medium">Misi</h2>
-                  <p class="tw-text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, ducimus nobis nihil minima reiciendis adipisci magnam eum laudantium pariatur cupiditate doloremque fugiat quae ab aliquam molestiae, sequi dolorum autem voluptates!</p>
+                    <div class="tw-overflow-x-auto">
+                        <table class="tw-w-full tw-text-lg tw-text-left tw-text-gray-900 tw-min-h-[400px]">
+                          <thead class="tw-text-lg tw-text-[#BC8947] tw-uppercase tw-bg-slate-200">
+                            <tr>
+                              <th scope="col" class="tw-pl-6 tw-py-3">No</th>
+                              <th scope="col" class="tw-pr-6 tw-py-3 tw-text-center">Misi</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr class="tw-bg-white tw-border-b tw-border-gray-400">
+                              <td class="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900">1</td>
+                              <td class="tw-px-6 tw-py-4">	
+                                Menyelenggarakan pendidikan di bidang Ilmu Teknik Informatika yang berkualitas dan relevan dengan tuntutan pengguna lulusan dalam rangka mencapai pembangunan nasional dan baku internasional.
+                            </tr>
+                            <tr class="tw-bg-white tw-border-b tw-border-gray-400 last:tw-border-0">
+                              <td class="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900">2</td>
+                              <td class="tw-px-6 tw-py-4">Menyelenggarakan pendidikan berkualitas di bidang informatika yang berorientasi pada perkembangan teknologi terkini dan kebutuhan industri.</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
                 </div>
               
                 <div class="tab-content tw-p-4 tw-hidden" id="tujuan">
-                  <h2 class="tw-text-3xl tw-text-[#BC8947] tw-mb-2 tw-font-medium">Tujuan</h2>
-                  <p class="tw-text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente rerum nihil reiciendis id quisquam corporis quae consequatur, expedita quas unde ut repudiandae dignissimos assumenda architecto asperiores, quam, blanditiis maiores maxime?</p>
-                </div>
+                    <div class="tw-overflow-x-auto">
+                        <table class="tw-w-full tw-text-lg tw-text-left tw-text-gray-900 tw-min-h-[400px]">
+                          <thead class="tw-text-lg tw-text-[#BC8947] tw-uppercase tw-bg-slate-200">
+                            <tr>
+                              <th scope="col" class="tw-pl-6 tw-py-3">No</th>
+                              <th scope="col" class="tw-pr-6 tw-py-3 tw-text-center">Tujuan</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr class="tw-bg-white tw-border-b tw-border-gray-400">
+                              <td class="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900">1</td>
+                              <td class="tw-px-6 tw-py-4">	
+                                Menghasilkan lulusan di bidang Teknik Informatika yang berkualitas, berdedikasi dan memiliki komitmen terhadap pembangunan nasional.</td>
+                            </tr>
+                            <tr class="tw-bg-white tw-border-b tw-border-gray-400 last:tw-border-0">
+                              <td class="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900">2</td>
+                              <td class="tw-px-6 tw-py-4">Menghasilkan lulusan yang kompeten di bidang informatika, kreatif, inovatif, dan mampu bersaing di tingkat nasional maupun internasional. 
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
               </div>
               
         </section>
